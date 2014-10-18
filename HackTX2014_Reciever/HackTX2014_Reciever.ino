76000993e668eddb7f0c245ba0fe610a520d598c
NoahPena <noahpena7@utexas.edu>
2014-10-18 15:43:34 -0500

    Added Some Code

--- HackTX2014_Reciever/HackTX2014_Reciever.ino
+++ HackTX2014_Reciever/HackTX2014_Reciever.ino
@@ -1,9 +1,43 @@
+#include <VirtualWire.h>
+
+int receivePin = 4;
+int motor = 3;
+uint8_t lastTen[10];
+int counter = 0;
+
 void setup()
 {
   Serial.begin(9600);
+  
+  vw_set_rx_pin(receivePin);
+  vw_set_ptt_inverted(true);
+  vw_setup(2000);
+  
+  vw_rx_start();
+  
+  pinMode(motor, OUTPUT);
+  
 }
 
 void loop()
 {
+  uint8_t buf[1];
+  uint8_t buflen = 1;
   
+  if(vw_get_message(buf, &buflen))
+  {
+    lastTen[counter] = buf[0];
+    counter++;
+    if(buf[0] == 0)
+    {
+     //Do Nothing 
+    }
+    
+    if(buf[0] == 1)
+    {
+     //Do Something 
+    }
+  } else {
+    
+  }
 }
--- HackTX2014_Sender/HackTX2014_Sender.ino
+++ HackTX2014_Sender/HackTX2014_Sender.ino
@@ -1,11 +1,33 @@
+#include <VirtualWire.h>
+
+int transmitPin = 7;
+int fsrPin = A0;
+int value = 0;
+
+int threshold = 0;
 
 void setup()
 {
   Serial.begin(9600);
   
+  vw_set_ptt_inverted(true);
+  vw_set_tx_pin(transmitPin);
+  vw_setup(2000);
+  
 }
 
 void loop()
 {
+  value = analogRead(fsrPin);
   
+  if(value > threshold)
+  {
+     vw_send(1, 1);
+     vw_wait_tx(); 
+  } else {
+     vw_send(0, 1);
+     vw_wait_tx(); 
+  }
+  
+  delay(500);
 }
